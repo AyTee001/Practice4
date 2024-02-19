be6a3d778180bada121c084e38c446c39fa7ea3d
AyTee001 <tiutiunanastasia@gmail.com>
2024-02-19 15:44:44 +0200

    Add scss

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+
+const scssFiles = './scss/*.scss';
+const outputCSS = './css/dist/';
+
+function compileSCSS() {
+  return gulp
+    .src(scssFiles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(outputCSS));
+}
+
+function watchSCSS() {
+  gulp.watch(scssFiles, compileSCSS);
+}
+
+exports.default = gulp.series(compileSCSS, watchSCSS);
